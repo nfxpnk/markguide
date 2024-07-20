07be19d62528a1e108636e0b5d3decc6af56027a
nfxpnk <nfxpnk@gmail.com>
2024-07-20 03:33:50 +0300

    components-prefix move to one single use file

--- src/config/config-optional.js
+++ src/config/config-optional.js
@@ -1,7 +1,30 @@
 'use strict';
 
+const fs = require('fs');
 const path = require('path');
-const getComponentsPrefix = require('./components-prefix.js');
+const log = require('fancy-log');
+const c = require('ansi-colors');
+
+function getComponentsPrefix(config) {
+    const prefixes = config.componentPrefixes;
+    let prefixExp = '';
+
+    if (prefixes && !Array.isArray(prefixes)) {
+        log(c.yellow('Warning: ') +
+            '"componentPrefixes" is defined, but it is not array. Default values used as fallback.');
+    }
+
+    if (Array.isArray(prefixes)) {
+        prefixes.forEach(function(prefix) { // could be id or class
+            prefixExp += `^.${prefix}|`;
+        });
+        prefixExp = prefixExp.replace(/\|$/g, '');
+    } else {
+        prefixExp = '^.b-|^.l-';
+    }
+
+    return new RegExp(prefixExp);
+}
 
 function getOptionalBaseConfigs(config) {
     let atlasConfig = {};
