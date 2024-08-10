5cdb6a2862c151af6aab25c7d124537e36623b2b
nfxpnk <nfxpnk@gmail.com>
2024-08-10 18:31:06 +0300

    nav - to - navigation

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -30,9 +30,9 @@ document.addEventListener('DOMContentLoaded', function () {
                 return false;
             }
 
-            end = element.classList.contains('b-markguide-nav');
+            end = element.classList.contains('b-markguide-navigation');
 
-            if (element.classList.contains('b-markguide-nav__item_category')) {
+            if (element.classList.contains('b-markguide-navigation__item_category')) {
                 element.querySelector('._category').classList.remove('js-collapsed');
             }
             findParent(element.parentElement);
@@ -67,7 +67,7 @@ document.addEventListener('DOMContentLoaded', function () {
         window.addEventListener('beforeunload', populateStorage);
 
         document.addEventListener('MyEventFired', function(event) {
-            const navItems = document.querySelectorAll(".b-markguide-nav__item-line");
+            const navItems = document.querySelectorAll(".b-markguide-navigation__item-line");
 
             navItems.forEach(item => {
                 const itemId = item.getAttribute("id");
