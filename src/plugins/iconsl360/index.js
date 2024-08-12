4210dd39caf008a90b472eb43262cf0bc946a8ba
nfxpnk <nfxpnk@gmail.com>
2024-08-13 00:22:33 +0300

    plugins

--- src/plugins/iconsl360/index.js
+++ src/plugins/iconsl360/index.js
@@ -9,13 +9,11 @@ class iconsl360Plugin extends basePlugin {
         super(config, options);
 
         // Path to the folder with icons files
-        if(path.isAbsolute(options.iconsFolder)) {
-            this.iconsFolder = options.iconsFolder;
+        if(path.isAbsolute(options.filePath)) {
+            this.filePath = options.filePath;
         } else {
-            this.iconsFolder = path.join(config.projectStaticFiles, options.iconsFolder, '/');
+            this.filePath = path.join(config.projectStaticFiles, options.filePath);
         }
-
-        this.fileExtension = options.fileExtension;
     }
 
     init() {
@@ -47,64 +45,15 @@ class iconsl360Plugin extends basePlugin {
     }
 
     getContent() {
-        const svgFiles = this.readSvgFiles();
-        const icons = svgFiles.map(file => this.extractSvgDetails(file));
         const template = this.getTemplate();
-        const htmlContent = mustache.render(template, { page: this.getConfiguration(), icons: icons });
+        const itemPartial = fs.readFileSync(path.join(__dirname, 'templates/item.mustache'), 'utf8');
+        const icons = JSON.parse(fs.readFileSync(this.filePath, 'utf8'));
+
+        const htmlContent = mustache.render(template, { page: this.getConfiguration(), icons: icons }, {item: itemPartial});
 
         return htmlContent;
     }
 
-    removeIscommentTags(svgContent) {
-        // Define the regular expression to match <iscomment> tags and their content
-        const regex = /<iscomment>[\s\S]*?<\/iscomment>/gi;
-        // Use the replace method to remove the matched content
-        return svgContent.replace(regex, '');
-    }
-
-    readSvgFiles() {
-        try {
-            return fs.readdirSync(this.iconsFolder).filter(file => file.endsWith('.' + this.fileExtension)).map(file => path.join(this.iconsFolder, file));
-        } catch (error) {
-            log.error(`Error reading SVG files from ${this.iconsFolder}: ${error.message}`);
-            return [];
-        }
-    }
-
-    extractSvgDetails(filePath) {
-        try {
-            let svgContent = fs.readFileSync(filePath, 'utf8');
-
-            svgContent = this.removeIscommentTags(svgContent);
-
-            const svgMatch = svgContent.match(/<svg[^>]*>/);
-            if (!svgMatch) throw new Error('Invalid SVG content');
-
-            const svgTag = svgMatch[0];
-            const widthMatch = svgTag.match(/width="(\d+)"/);
-            const heightMatch = svgTag.match(/height="(\d+)"/);
-            const viewBoxMatch = svgTag.match(/viewBox="(\d+ \d+ \d+ \d+)"/);
-
-            const width = widthMatch ? widthMatch[1] : null;
-            const height = heightMatch ? heightMatch[1] : null;
-            const viewBox = viewBoxMatch ? viewBoxMatch[1].split(' ') : [null, null, null, null];
-            const viewBoxWidth = viewBox[2];
-            const viewBoxHeight = viewBox[3];
-
-            return {
-                svg: svgContent,
-                width,
-                height,
-                viewBoxWidth,
-                viewBoxHeight,
-                name: path.basename(filePath),
-                filename: path.basename(filePath)
-            };
-        } catch (error) {
-            log.error(`Error extracting details from SVG file ${filePath}: ${error.message}`);
-            return null;
-        }
-    }
 
     getTemplate() {
         try {
