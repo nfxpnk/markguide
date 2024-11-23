dcd37b586f90cedf2bbc7cbce6574ea38161a497
nfxpnk <nfxpnk@gmail.com>
2024-11-23 17:59:03 +0200

    build date

--- assets/js/build-date.js
+++ assets/js/build-date.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function () {
     (function () {
         function loadBuildDateScript() {
             const script = document.createElement('script');
-            script.src = '/build-date.js'; // Path to the build-date.js file
+            script.src = 'build-date.js'; // Path to the build-date.js file
             script.onload = () => {
                 const buildDateElement = document.getElementById('build-date');
                 buildDateElement.textContent = buildDateString;
